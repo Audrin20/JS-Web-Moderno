b719229dd1f1b40f52279333ba3cbc1ffce3581c
Audrin20 <audrinh@gmail.com>
2021-03-02 00:33:47 -0300

    Começando pelos fundamentos

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de codigo")
+
+/* console.log-> Output default javascript */
+
+{
+    {
+        console.log("olá");
+        console.log('Mundo!') // Padrão do Curso
+    }
+}
