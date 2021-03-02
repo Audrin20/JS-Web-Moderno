be220a63eaa6bb83d2ebc7b9a2c1084dda367ea4
Audrin20 <audrinh@gmail.com>
2021-03-02 00:56:52 -0300

    fundamentos/variaveisEConstantes.js

---
+++ fundamentos/variabeisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3 //
+let b = 4 // É melhor usar let pra criação de variável
+
+var a = 30 // no mesmo escopo a variável pode ser re-declarada
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50
+console.log(c)
