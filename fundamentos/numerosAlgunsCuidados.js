3fc46940d61a035ed01cfb1650ebb1fccabd0736
Audrin20 <audrinh@gmail.com>
2021-03-02 01:20:43 -0300

    fundamentos/numerosAlgunsCuidados.js

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0) // Infinity
+console.log("10"/2) // converte a string para inteiro e divide
+console.log("Show!" * 2)
+console.log(0.1 + 0.7) // Não é 0.8
+//console.log(10.toString()) // Dá ruim
+console.log((10.4).toString()) // dá bom
+console.log((10.4).toFixed(12)) // dá bom
