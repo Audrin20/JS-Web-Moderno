651091a24780b96e7f7a99eb30d7911aa65e95d1
Audrin20 <audrinh@gmail.com>
2021-03-02 08:45:54 -0300

    Objetos.js

---
+++ fundamentos/objeto.js
@@ -0,0 +1,19 @@
+const prod1 = {} // "Objeto = {}" Vazio {} Pares "Chave: valor"
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 // Não use atributos scom espaços
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+    // obj:{
+    //    blabla: 1,
+    //    obj: {
+    //        blabla: 2
+    //    }
+    // }
+}
+
+'{nome":"Camisa Polo", "preco": "79.90"}'
+console.log(prod2)
