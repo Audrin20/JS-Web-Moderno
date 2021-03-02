8f9de1bf744447ba4ce49e78dc3ff8696b35a5c6
Audrin20 <audrinh@gmail.com>
2021-03-02 13:03:57 -0300

    Usando Var

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,8 @@
+{{{{ var sera = 'Será???'}}}}
+console.log(sera)
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+console.log(local) // não funciona pq a variável é local dentro da funcao 
