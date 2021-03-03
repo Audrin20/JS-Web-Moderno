7a5327eae15a0c1a10448f9b5df0eaaefb667754
Audrin20 <audrinh@gmail.com>
2021-03-02 23:08:45 -0300

    Escopo de variável let e var

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,8 @@
+// quando declara var o proprio interpretador joga a variável pra cima -> isso chama-se hoisting
+console.log(a)
+var a = 2
+console.log(a)
+
+console.log(b)
+let b = 2
+console.log(b)
