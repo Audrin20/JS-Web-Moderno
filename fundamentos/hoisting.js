85d8381fc921938d6a5764e42704ec4664515cc3
Audrin20 <audrinh@gmail.com>
2021-03-02 23:17:08 -0300

    Objeto e Hoisting

--- fundamentos/hoisting.js
+++ fundamentos/hoisting.js
@@ -1,8 +1,9 @@
 // quando declara var o proprio interpretador joga a variável pra cima -> isso chama-se hoisting
+// HOISTING -> move a declaração da variavel para o topo
 console.log(a)
 var a = 2
 console.log(a)
 
-console.log(b)
-let b = 2
-console.log(b)
+// console.log(b)
+// let b = 2 // hoisting não ocorre aqui
+// console.log(b)
