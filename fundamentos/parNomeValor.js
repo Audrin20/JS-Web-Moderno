19f227989ae93839bd77e10c791936d71d655a4e
Audrin20 <audrinh@gmail.com>
2021-03-03 11:21:32 -0300

    Par/Valor

---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,20 @@
+// par nome/valor
+const saudacao = 'Opa' // contexto léxico 1
+
+function exec() {
+	const saudacao = 'Falaa' // contexto léxico 2
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const Cliente = {
+	nome: 'Pedro',
+	idade: 32,
+	peso: 90,
+	endereco: {
+		logradouro: 'Rua Muito Legal',
+		numero: 123
+	}
+}
+
+console.log(saudacao)
+console.log(exec())
