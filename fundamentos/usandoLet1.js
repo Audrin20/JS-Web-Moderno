0cdcf5fa56d3db20110fc0733ff83a551389955c
Audrin20 <audrinh@gmail.com>
2021-03-02 22:46:54 -0300

    usando Let

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+let numero = 1
+{
+    let numero2 = 2
+    console.log('dentro =', numero)
+}
+console.log('fora=', numero)
+
+// variáveis definidas com -> "var" tem escopo global e de funcao
+// variáveis definidas com -> "let" escopo global, funcao e bloco
