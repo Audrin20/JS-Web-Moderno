ba32926e3c1e4c419c499f8ed110167be5214d6b
Audrin20 <audrinh@gmail.com>
2021-03-12 09:11:59 -0300

    explicação sobre Orientação a Objetos

---
+++ objeto/oo.js
@@ -0,0 +1,25 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+// AULA CONCEITUAL
+//
+// Paradigma Procedural ( Função )
+// Código reutilizável
+processamento(valor1, valor2, valor3)
+// procedimento recebe dados
+//
+// OO dados e dentro dos dados, tem funções
+objeto = {
+	valor1,
+	valor2,
+	valor3,
+	processamento() {
+		// ...
+	}
+}
+
+objeto.processamento() // Foco passo a passo
+// Princípios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo ( multiplas formas )
+
