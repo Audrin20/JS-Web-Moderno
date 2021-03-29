b752c5c2c655bb6364c6262a0eba404ef2e935dc
Audrin20 <audrinh@gmail.com>
2021-03-29 08:21:30 -0300

    concat e imperativoVsDeclarativo

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhas.concat(filhos, 'Fulano')
+console.group(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6,7]]))
